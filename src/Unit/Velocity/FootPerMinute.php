6a54fe54b34e2722822886ce9e9b7a8e61015347
Cameron Pavey <cpave3@gmail.com>
2019-05-01 15:21:34 +1000

    fix: Added correct unit systems to velocity units

--- src/Unit/Velocity/FootPerMinute.php
+++ src/Unit/Velocity/FootPerMinute.php
@@ -1,10 +1,11 @@
 <?php
 namespace PhpUnitConversion\Unit\Velocity;
 
-use PhpUnitConversion\System\Metric;
+use PhpUnitConversion\System\Imperial;
+use PhpUnitConversion\System\USC;
 use PhpUnitConversion\Unit\Velocity;
 
-class FootPerMinute extends Velocity implements Metric
+class FootPerMinute extends Velocity implements Imperial, USC
 {
     const FACTOR = 1/196.85;
 
--- src/Unit/Velocity/InchPerHour.php
+++ src/Unit/Velocity/InchPerHour.php
@@ -1,10 +1,11 @@
 <?php
 namespace PhpUnitConversion\Unit\Velocity;
 
-use PhpUnitConversion\System\Metric;
+use PhpUnitConversion\System\Imperial;
+use PhpUnitConversion\System\USC;
 use PhpUnitConversion\Unit\Velocity;
 
-class InchPerHour extends Velocity implements Metric
+class InchPerHour extends Velocity implements Imperial, USC
 {
     const FACTOR = 1/141732.3;
 
